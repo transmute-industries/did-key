bd1f0ec0d1b55dd81c5928fa266f97162183b476
Orie Steele <orie@ohs.io>
2019-11-04 20:57:45 -0600

    update demo

--- packages/pwa/src/App.js
+++ packages/pwa/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Ed25519KeyPair } from "crypto-ld";
+import didMethodKey from 'did-method-key';
+import "./App.css";
+
+const { keyToDidDoc } = didMethodKey.driver();
+
+
+class App extends React.Component{
+  state = {key : null}
+  async componentDidMount(){
+    const edKey = await Ed25519KeyPair.generate();
+    const doc = keyToDidDoc(edKey);
+    this.setState({
+      key: edKey,
+      doc,
+      resolverUrl: "https://did-key.web.app/api/dids/" + doc.id
+    })
+  }
+  render(){
+    if (!this.state.doc){
+      return 'generating...'
+    }
+      return (
+        <div className="App">
+          <div>
+          <a href="https://did-key.web.app/api/docs">Swagger API</a> &nbsp;|&nbsp;
+          <a href="https://did-key.web.app/schemas/com.transmute.did/didDocument/jsonschema/1-0-0.jsonld">JSON Schema</a>
+          </div>
+         
+          <h4>Ed25519KeyPair</h4>
+          <pre>{JSON.stringify(this.state.key, null, 2)}</pre>
+          <h4><a href={this.state.resolverUrl}>{this.state.resolverUrl}</a></h4>
+          <pre>{JSON.stringify(this.state.doc, null, 2)}</pre>
+        </div>
+      );
+  }
 }
 
 export default App;
